0b4648d70cb3da4177e587b1382395998a93fa81
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-03-14 00:51:37 +0330

    news entity

---
+++ src/main/java/ir/hamyiar/newstb/dao/News.java
@@ -0,0 +1,29 @@
+package ir.hamyiar.newstb.dao;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "news")
+@Data
+public class News {
+    @Id
+    @Column(name = "id")
+    private String id;
+    @Column(name = "pre_id")
+    private String preId;
+    @Column(name = "site_title")
+    private String siteTitle;
+    @Column(name = "site_address")
+    private String siteAddress;
+    @Column(name = "news_title")
+    private String newsTitle;
+    @Column(name = "news_link")
+    private String newsLink;
+    @Column(name = "news_description")
+    private String newsDescription;
+}
