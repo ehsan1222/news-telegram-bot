4beeab6200c4f9fd9c44fce9adfadd807dab4aa5
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-03-14 01:10:58 +0330

    enable schedule methods

--- src/main/java/ir/hamyiar/newstb/NewsTelegramBotApplication.java
+++ src/main/java/ir/hamyiar/newstb/NewsTelegramBotApplication.java
@@ -1,9 +1,12 @@
 package ir.hamyiar.newstb;
 
+import ir.hamyiar.newstb.services.IrnaRSSService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class NewsTelegramBotApplication {
 
 	public static void main(String[] args) {
