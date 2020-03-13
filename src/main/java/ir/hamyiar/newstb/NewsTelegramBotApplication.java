ad3068a37efeb40db9ed743dbe9b7cc5609589ab
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-03-13 19:03:54 +0330

    init project with spring boot initializr

---
+++ src/main/java/ir/hamyiar/newstb/NewsTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package ir.hamyiar.newstb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ir/hamyiar/newstb/NewsTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.hamyiar.newstb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
