05c03e25c6b5d1d2d3ed8fe257e87271794e35ef
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-03-14 04:55:56 +0330

    update import in News entity

--- src/main/java/ir/hamyiar/newstb/dao/News.java
+++ src/main/java/ir/hamyiar/newstb/dao/News.java
@@ -6,7 +6,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import java.sql.Date;
+import java.util.Date;
 
 @Entity
 @Table(name = "news")
