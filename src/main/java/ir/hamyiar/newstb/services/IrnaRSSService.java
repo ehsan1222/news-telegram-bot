875564459d4df807e2f81ae8b69a362dddcd2ae0
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-03-14 23:59:41 +0330

    Store didn't exist news to database

--- src/main/java/ir/hamyiar/newstb/services/IrnaRSSService.java
+++ src/main/java/ir/hamyiar/newstb/services/IrnaRSSService.java
@@ -3,6 +3,8 @@ package ir.hamyiar.newstb.services;
 import ir.hamyiar.newstb.dao.News;
 import ir.hamyiar.newstb.helper.XmlHelper;
 import ir.hamyiar.newstb.net.NewsConnection;
+import ir.hamyiar.newstb.repository.NewsRepository;
+import org.springframework.beans.factory.annotation.Autowire;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +20,9 @@ public class IrnaRSSService {
     @Autowired
     private XmlHelper xmlHelper;
 
+    @Autowired
+    private NewsRepository newsRepository;
+
     @PostConstruct
     public void process() {
         // Download RSS Data from site
@@ -25,7 +30,20 @@ public class IrnaRSSService {
         // Parse downloaded file and convert to News Entity
         List<News> todayNews = xmlHelper.parse(xmlRSS);
         addPreIdToNewsId(todayNews);
+        // TODO: Check this news id is not exist in data base then store it
+        // TODO: if news is is not in database then send to telegram channel
+        // TODO: Remove yesterday news in database
+        checkNewsInDatabase(todayNews);
+    }
 
+    private void checkNewsInDatabase(List<News> todayNews) {
+        for (News news : todayNews) {
+            if (!newsRepository.findById(news.getId()).isPresent()) {
+                // Store News Entity in Database
+                newsRepository.save(news);
+                // TODO: Send data to telegram channel
+            }
+        }
     }
 
     public void addPreIdToNewsId(List<News> newsList) {
