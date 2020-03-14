66af69a798a7b084c53bf634db4d62ac2765a783
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-03-15 00:49:26 +0330

    Find profile.properties injection

--- src/main/java/ir/hamyiar/newstb/NewsTelegramBotApplication.java
+++ src/main/java/ir/hamyiar/newstb/NewsTelegramBotApplication.java
@@ -2,10 +2,12 @@ package ir.hamyiar.newstb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@PropertySource(value = {"classpath:profile.properties"})
 public class NewsTelegramBotApplication {
 
 	public static void main(String[] args) {
