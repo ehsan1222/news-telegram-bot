f5136660b27a4119ab1a2c82d2d02ecdbc44cf8e
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-03-15 16:32:42 +0330

    Enable transaction annotation

--- src/main/java/ir/hamyiar/newstb/NewsTelegramBotApplication.java
+++ src/main/java/ir/hamyiar/newstb/NewsTelegramBotApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableTransactionManagement
 @PropertySource(value = {"classpath:profile.properties"})
 public class NewsTelegramBotApplication {
 
